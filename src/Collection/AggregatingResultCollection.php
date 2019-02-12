bab4365e753779b991d398910ba49d30fb7cf1d8
BluePsyduck <bluepsyduck@gmx.com>
2019-02-12 18:50:37 +0100

    Added a collection able to aggregate and merge the search results.

---
+++ src/Collection/AggregatingResultCollection.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FactorioItemBrowser\Api\Search\Collection;
+
+use FactorioItemBrowser\Api\Search\Entity\Result\ItemResult;
+use FactorioItemBrowser\Api\Search\Entity\Result\RecipeResult;
+use FactorioItemBrowser\Api\Search\Entity\Result\ResultInterface;
+
+/**
+ * The collection aggregating results.
+ *
+ * @author BluePsyduck <bluepsyduck@gmx.com>
+ * @license http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+class AggregatingResultCollection
+{
+    /**
+     * The aggregated items.
+     * @var ItemCollection
+     */
+    protected $items;
+
+    /**
+     * The aggregated recipes.
+     * @var RecipeCollection
+     */
+    protected $recipes;
+
+    /**
+     * Initializes the collection.
+     */
+    public function __construct()
+    {
+        $this->items = new ItemCollection();
+        $this->recipes = new RecipeCollection();
+    }
+
+    /**
+     * Adds an item to the collection.
+     * @param ItemResult $item
+     * @return $this
+     */
+    public function addItem(ItemResult $item): self
+    {
+        $this->items->add($item);
+        return $this;
+    }
+
+    /**
+     * Removes the specified item from the collection.
+     * @param ItemResult $item
+     * @return $this
+     */
+    public function removeItem(ItemResult $item): self
+    {
+        $this->items->remove($item);
+        return $this;
+    }
+
+    /**
+     * Returns the items from the collection.
+     * @return array|ItemResult[]
+     */
+    public function getItems(): array
+    {
+        return $this->items->getAll();
+    }
+
+    /**
+     * Adds an recipe to the collection.
+     * @param RecipeResult $recipe
+     * @return $this
+     */
+    public function addRecipe(RecipeResult $recipe): self
+    {
+        $this->recipes->add($recipe);
+        return $this;
+    }
+
+    /**
+     * Removes the specified recipe from the collection.
+     * @param RecipeResult $recipe
+     * @return $this
+     */
+    public function removeRecipe(RecipeResult $recipe): self
+    {
+        $this->recipes->remove($recipe);
+        return $this;
+    }
+
+    /**
+     * Returns the recipes from the collection.
+     * @return array|RecipeResult[]
+     */
+    public function getRecipes(): array
+    {
+        return $this->recipes->getAll();
+    }
+
+    /**
+     * Returns the merged and sorted results of the collection.
+     * @return array|ResultInterface[]
+     */
+    public function getMergedResults(): array
+    {
+        $allResults = array_merge(
+            $this->items->getAll(),
+            $this->recipes->getAll()
+        );
+
+        usort($allResults, [$this, 'compareResults']);
+
+        return $allResults;
+    }
+
+    /**
+     * Compares the two specified results.
+     * @param ResultInterface $left
+     * @param ResultInterface $right
+     * @return int
+     */
+    protected function compareResults(ResultInterface $left, ResultInterface $right): int
+    {
+        $leftCriteria = $this->getSortCriteria($left);
+        $rightCriteria = $this->getSortCriteria($right);
+
+        $result = 0;
+        while ($result === 0 && count($leftCriteria) > 0) {
+            $result = array_shift($leftCriteria) <=> array_shift($rightCriteria);
+        }
+        return $result;
+    }
+
+    /**
+     * Returns the sort criteria for the specified result.
+     * @param ResultInterface $result
+     * @return array
+     */
+    protected function getSortCriteria(ResultInterface $result): array
+    {
+        return [
+            $result->getPriority(),
+            $result->getName(),
+            $result->getType(),
+        ];
+    }
+}
---
+++ test/src/Collection/AggregatingResultCollectionTest.php
@@ -0,0 +1,333 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FactorioItemBrowserTest\Api\Search\Collection;
+
+use BluePsyduck\Common\Test\ReflectionTrait;
+use FactorioItemBrowser\Api\Search\Collection\AggregatingResultCollection;
+use FactorioItemBrowser\Api\Search\Collection\ItemCollection;
+use FactorioItemBrowser\Api\Search\Collection\RecipeCollection;
+use FactorioItemBrowser\Api\Search\Entity\Result\ItemResult;
+use FactorioItemBrowser\Api\Search\Entity\Result\RecipeResult;
+use FactorioItemBrowser\Api\Search\Entity\Result\ResultInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+
+/**
+ * The PHPUnit test of the AggregatingResultCollection class.
+ *
+ * @author BluePsyduck <bluepsyduck@gmx.com>
+ * @license http://opensource.org/licenses/GPL-3.0 GPL v3
+ * @coversDefaultClass \FactorioItemBrowser\Api\Search\Collection\AggregatingResultCollection
+ */
+class AggregatingResultCollectionTest extends TestCase
+{
+    use ReflectionTrait;
+
+    /**
+     * Tests the constructing.
+     * @throws ReflectionException
+     * @covers ::__construct
+     */
+    public function testConstruct(): void
+    {
+        $collection = new AggregatingResultCollection();
+
+        $this->assertInstanceOf(ItemCollection::class, $this->extractProperty($collection, 'items'));
+        $this->assertInstanceOf(RecipeCollection::class, $this->extractProperty($collection, 'recipes'));
+    }
+
+    /**
+     * Tests the addItem method.
+     * @throws ReflectionException
+     * @covers ::addItem
+     */
+    public function testAddItem(): void
+    {
+        /* @var ItemResult&MockObject $item */
+        $item = $this->createMock(ItemResult::class);
+
+        /* @var ItemCollection&MockObject $itemCollection */
+        $itemCollection = $this->createMock(ItemCollection::class);
+        $itemCollection->expects($this->once())
+                       ->method('add')
+                       ->with($this->identicalTo($item));
+
+        $collection = new AggregatingResultCollection();
+        $this->injectProperty($collection, 'items', $itemCollection);
+
+        $result = $collection->addItem($item);
+
+        $this->assertSame($collection, $result);
+    }
+
+    /**
+     * Tests the removeItem method.
+     * @throws ReflectionException
+     * @covers ::removeItem
+     */
+    public function testRemoveItem(): void
+    {
+        /* @var ItemResult&MockObject $item */
+        $item = $this->createMock(ItemResult::class);
+
+        /* @var ItemCollection&MockObject $itemCollection */
+        $itemCollection = $this->createMock(ItemCollection::class);
+        $itemCollection->expects($this->once())
+                       ->method('remove')
+                       ->with($this->identicalTo($item));
+
+        $collection = new AggregatingResultCollection();
+        $this->injectProperty($collection, 'items', $itemCollection);
+
+        $result = $collection->removeItem($item);
+
+        $this->assertSame($collection, $result);
+    }
+
+    /**
+     * Tests the getItems method.
+     * @throws ReflectionException
+     * @covers ::getItems
+     */
+    public function testGetItems(): void
+    {
+        $items = [
+            $this->createMock(ItemResult::class),
+            $this->createMock(ItemResult::class),
+        ];
+
+        /* @var ItemCollection&MockObject $itemCollection */
+        $itemCollection = $this->createMock(ItemCollection::class);
+        $itemCollection->expects($this->once())
+                       ->method('getAll')
+                       ->willReturn($items);
+
+        $collection = new AggregatingResultCollection();
+        $this->injectProperty($collection, 'items', $itemCollection);
+
+        $result = $collection->getItems();
+
+        $this->assertSame($items, $result);
+    }
+    
+    /**
+     * Tests the addRecipe method.
+     * @throws ReflectionException
+     * @covers ::addRecipe
+     */
+    public function testAddRecipe(): void
+    {
+        /* @var RecipeResult&MockObject $recipe */
+        $recipe = $this->createMock(RecipeResult::class);
+
+        /* @var RecipeCollection&MockObject $recipeCollection */
+        $recipeCollection = $this->createMock(RecipeCollection::class);
+        $recipeCollection->expects($this->once())
+                         ->method('add')
+                         ->with($this->identicalTo($recipe));
+
+        $collection = new AggregatingResultCollection();
+        $this->injectProperty($collection, 'recipes', $recipeCollection);
+
+        $result = $collection->addRecipe($recipe);
+
+        $this->assertSame($collection, $result);
+    }
+
+    /**
+     * Tests the removeRecipe method.
+     * @throws ReflectionException
+     * @covers ::removeRecipe
+     */
+    public function testRemoveRecipe(): void
+    {
+        /* @var RecipeResult&MockObject $recipe */
+        $recipe = $this->createMock(RecipeResult::class);
+
+        /* @var RecipeCollection&MockObject $recipeCollection */
+        $recipeCollection = $this->createMock(RecipeCollection::class);
+        $recipeCollection->expects($this->once())
+                         ->method('remove')
+                         ->with($this->identicalTo($recipe));
+
+        $collection = new AggregatingResultCollection();
+        $this->injectProperty($collection, 'recipes', $recipeCollection);
+
+        $result = $collection->removeRecipe($recipe);
+
+        $this->assertSame($collection, $result);
+    }
+
+    /**
+     * Tests the getRecipes method.
+     * @throws ReflectionException
+     * @covers ::getRecipes
+     */
+    public function testGetRecipes(): void
+    {
+        $recipes = [
+            $this->createMock(RecipeResult::class),
+            $this->createMock(RecipeResult::class),
+        ];
+
+        /* @var RecipeCollection&MockObject $recipeCollection */
+        $recipeCollection = $this->createMock(RecipeCollection::class);
+        $recipeCollection->expects($this->once())
+                         ->method('getAll')
+                         ->willReturn($recipes);
+
+        $collection = new AggregatingResultCollection();
+        $this->injectProperty($collection, 'recipes', $recipeCollection);
+
+        $result = $collection->getRecipes();
+
+        $this->assertSame($recipes, $result);
+    }
+    
+    /**
+     * Tests the getMergedResults method.
+     * @throws ReflectionException
+     * @covers ::getMergedResults
+     */
+    public function testGetMergedResults(): void
+    {
+        /* @var ItemResult&MockObject $item */
+        $item = $this->createMock(ItemResult::class);
+        /* @var RecipeResult&MockObject $recipe */
+        $recipe = $this->createMock(RecipeResult::class);
+
+        /* @var ItemCollection&MockObject $itemCollection */
+        $itemCollection = $this->createMock(ItemCollection::class);
+        $itemCollection->expects($this->once())
+                       ->method('getAll')
+                       ->willReturn([$item]);
+
+        /* @var RecipeCollection&MockObject $recipeCollection */
+        $recipeCollection = $this->createMock(RecipeCollection::class);
+        $recipeCollection->expects($this->once())
+                         ->method('getAll')
+                         ->willReturn([$recipe]);
+
+        /* @var AggregatingResultCollection&MockObject $collection */
+        $collection = $this->getMockBuilder(AggregatingResultCollection::class)
+                           ->setMethods(['compareResults'])
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $collection->expects($this->once())
+                   ->method('compareResults')
+                   ->with($this->identicalTo($item), $this->identicalTo($recipe))
+                   ->willReturn(1);
+        $this->injectProperty($collection, 'items', $itemCollection);
+        $this->injectProperty($collection, 'recipes', $recipeCollection);
+
+        $result = $collection->getMergedResults();
+
+        $this->assertEquals([$recipe, $item], $result);
+    }
+    
+
+    /**
+     * Provides the data for the compareResults test.
+     * @return array
+     */
+    public function provideCompareResults(): array
+    {
+        return [
+            [
+                [21, 'abc', 'def'],
+                [42, 'abc', 'def'],
+                -1,
+            ],
+            [
+                [42, 'abc', 'def'],
+                [21, 'abc', 'def'],
+                1,
+            ],
+            [
+                [42, 'abc', 'def'],
+                [42, 'def', 'abc'],
+                -1,
+            ],
+            [
+                [42, 'def', 'abc'],
+                [42, 'abc', 'def'],
+                1,
+            ],
+            [
+                [42, 'abc', 'def'],
+                [42, 'abc', 'def'],
+                0,
+            ],
+        ];
+    }
+
+    /**
+     * Tests the compareResults method.
+     * @param array $leftCriteria
+     * @param array $rightCriteria
+     * @param int $expectedResult
+     * @throws ReflectionException
+     * @covers ::compareResults
+     * @dataProvider provideCompareResults
+     */
+    public function testCompareResults(array $leftCriteria, array $rightCriteria, int $expectedResult): void
+    {
+        /* @var ResultInterface&MockObject $leftResult */
+        $leftResult = $this->createMock(ResultInterface::class);
+        /* @var ResultInterface&MockObject $rightResult */
+        $rightResult = $this->createMock(ResultInterface::class);
+
+        /* @var AggregatingResultCollection&MockObject $collection */
+        $collection = $this->getMockBuilder(AggregatingResultCollection::class)
+                           ->setMethods(['getSortCriteria'])
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $collection->expects($this->exactly(2))
+                   ->method('getSortCriteria')
+                   ->withConsecutive(
+                       [$this->identicalTo($leftResult)],
+                       [$this->identicalTo($rightResult)]
+                   )
+                   ->willReturnOnConsecutiveCalls(
+                       $leftCriteria,
+                       $rightCriteria
+                   );
+
+        $result = $this->invokeMethod($collection, 'compareResults', $leftResult, $rightResult);
+
+        $this->assertSame($expectedResult, $result);
+    }
+
+    /**
+     * Tests the getSortCriteria method.
+     * @throws ReflectionException
+     * @covers ::getSortCriteria
+     */
+    public function testGetSortCriteria(): void
+    {
+        $priority = 42;
+        $name = 'abc';
+        $type = 'def';
+        $expectedResult = [42, 'abc', 'def'];
+
+        /* @var ResultInterface&MockObject $entity */
+        $entity = $this->createMock(ResultInterface::class);
+        $entity->expects($this->once())
+               ->method('getPriority')
+               ->willReturn($priority);
+        $entity->expects($this->once())
+               ->method('getName')
+               ->willReturn($name);
+        $entity->expects($this->once())
+               ->method('getType')
+               ->willReturn($type);
+
+        $collection = new AggregatingResultCollection();
+        $result = $this->invokeMethod($collection, 'getSortCriteria', $entity);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+}
